c787b1de17dcf844dc3a12e4f85a51541d0fb094
Wasit Mirani <44178287+wasitmirani@users.noreply.github.com>
2022-05-27 00:32:31 +0500

    first commit

---
+++ models/User.ts
@@ -0,0 +1,9 @@
+import { Request, Response, NextFunction } from 'express';
+
+export class  User {
+    
+      getAllUsers = (request: Request, response: Response, next: NextFunction) => {
+
+          return null
+      };
+}
